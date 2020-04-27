81ef38f8a690ca2f4fe7ffc861776171783b6cfe
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-04-27 20:09:28 +0100

    404

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String, 
     required: [true, "A Tour must have a name"], 
-    unique: true
+    unique: true, 
+    trim: true, 
+    maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+    minlength: [5, 'A tour name must have more or equal than 5 characters']
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, "A Tour must have a duration"] 
@@ -16,11 +21,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String, 
-    required: [true, "A Tour must have a Difficulty"]
+    required: [true, "A Tour must have a Difficulty"], 
+    enum: {
+      values: ['easy', 'medium', 'difficult'], 
+      message: "Difficulty is either: easy, medium, difficult"
+    }
   },
   ratingsAverage: {
     type: Number, 
-    default: 4
+    default: 4,
+    min: [1, 'Rating must be above 1'],
+    max: [5, 'Rating must be below 5']
   },
   ratingsQuantity: {
     type: Number, 
@@ -30,7 +41,15 @@ const tourSchema = new mongoose.Schema({
     type: Number, 
     required: [true, "A Tour must have a price"]
   }, 
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        return val < this.price
+    }, 
+    message: "Discount Price ({VALUE}) should be below regular price"
+  }
+  },
   summary: {
     type: String, 
     trim: true,
@@ -50,9 +69,52 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(), 
     select: false
   },
-  startDates: [Date]
+  startDates: [Date], 
+  secretTour: {
+    type: Boolean, 
+    default: false
+  }
+}, {
+  toJSON: { virtuals: true }, 
+  toObject: { virtuals: true }
+})
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7
+})
+
+// Document MiddlesWare runs before save and create
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true})
+  next()
+})
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc)
+//   next()
+// })
+
+// Query Middleware - done before any find query
+tourSchema.pre(/^find/, function(next) {
+  this.find({secretTour: { $ne: true }})
+
+  this.start = Date.now()
+  next()
+})
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`)
+  next()
+})
+
+// Aggregation Middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { $ne: true } })
+  next()
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
 
 module.exports = Tour;
+
+
---
+++ notes/error-handling.md
@@ -0,0 +1,12 @@
+### Operational Errors 
+
+Problems that we can predict will happen at some point, so we just need to handle them in advance. 
+
+- Invalid Path Accessed
+- Invalid User Input 
+- Failed to connect to server 
+- Failed to connect to DB
+- Request Timeout
+
+### Programming Errors
+
