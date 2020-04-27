afc3075b1b55f601ae4c887bb528cec830fb34ee
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-04-27 17:04:55 +0100

    api

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const tourController = require('../controllers/tourController')
 
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+  
 router.route('/').get(tourController.getAllTours).post(tourController.createTour)
 router.route('/:id').get(tourController.getTourById).patch(tourController.updateTour).delete(tourController.deleteTour)
 
